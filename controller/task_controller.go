8c20431859ffa6a79a46c888f6f45ecb2fc144ff
takumu <takumu155695@gmail.com>
2023-12-19 07:54:17 +0900

    TaskのStatus機能を追加

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -15,6 +15,7 @@ type ITaskController interface {
 	GetTaskById(c echo.Context) error
 	CreateTask(c echo.Context) error
 	UpdateTask(c echo.Context) error
+	UpdateTaskStatus(c echo.Context) error
 	DeleteTask(c echo.Context) error
 }
 
@@ -85,6 +86,25 @@ func (tc *taskController) UpdateTask(c echo.Context) error {
 	taskRes, err := tc.tu.UpdateTask(task, uint(userId.(float64)), uint(taskId))
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, taskRes)
+}
+
+func (tc *taskController) UpdateTaskStatus(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+	id := c.Param(("taskId"))
+	taskId, _ := strconv.Atoi(id)
+
+	task := model.Task{}
+	if err := c.Bind(&task); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	taskRes, err := tc.tu.UpdateTaskStatus(task, uint(userId.(float64)), uint(taskId))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
 	}
 
 	return c.JSON(http.StatusOK, taskRes)
--- model/task.go
+++ model/task.go
@@ -3,19 +3,28 @@ package model
 import "time"
 
 type Task struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title" gorm:"not null"`
-	OnGoing   bool      `json:"on_going" gorm:"default:false"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	User      User      `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
-	UserId    uint      `json:"userId" gorm:"not null"`
+	ID        uint       `json:"id" gorm:"primaryKey"`
+	Title     string     `json:"title" gorm:"not null"`
+	Status    TaskStatus `json:"status" gorm:"not null; default:0"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	User      User       `json:"user" gorm:"foreignKey:UserId; constraint:OnDelete:CASCADE"`
+	UserId    uint       `json:"userId" gorm:"not null"`
 }
 
 type TaskResponse struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title" gorm:"not null"`
-	OnGoing   bool      `json:"on_going" gorm:"default:false"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint       `json:"id" gorm:"primaryKey"`
+	Title     string     `json:"title" gorm:"not null"`
+	Status    TaskStatus `json:"status" gorm:"not null; default:TaskStatusUnstarted"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
 }
+
+type TaskStatus int
+
+const (
+	TaskStatusUnstarted TaskStatus = iota
+	TaskStatusStarted
+	TaskStatusCompleted
+)
+
